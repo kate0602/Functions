50c4785132b019fc51adc02b657e3c4bc96ca36d
kate0602 <burm-ekaterina@yandex.ru>
2023-04-23 21:06:36 +0400

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+using namespace std;
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+void Sum(int arr[], const int n);
+void Avg(int arr[], const int n);
+void minValueIn(int arr[], const int n);
+void maxValueIn(int arr[], const int n);
+void shiftLeft(int arr[], const int n);
+void shiftRight(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+	Sum(arr, n);
+	Avg(arr, n);
+	minValueIn(arr, n);
+	maxValueIn(arr, n);
+	Sort(arr, n);
+	cout << endl;
+	Print(arr, n);
+	Sum(arr, n);
+	Avg(arr, n);
+	minValueIn(arr, n);
+	maxValueIn(arr, n);
+	cout << endl;
+
+	const int SIZE = 8;
+	int brr[SIZE];
+
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	shiftLeft(brr, SIZE);
+	Print(brr, SIZE);
+	Sort(brr, SIZE);
+	Print(brr, SIZE);
+	shiftRight(brr, SIZE);
+	Print(brr, SIZE);
+
+	
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "сумма элементов массива: " << sum;
+	cout << endl;
+}
+void Avg(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	int arithmeticmean = sum / n;
+	cout << "среднее арифметическое элементов массива: " << arithmeticmean;
+	cout << endl;
+}
+void minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (min > arr[i])
+			min = arr[i];
+
+	}
+	cout << "Минимальное значение: " << min << endl;
+}
+void maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+
+	for (int i = 1; i < n; i++)
+	{
+
+		if (max < arr[i])
+			max = arr[i];
+	}
+
+	cout << "Максммальное значение: " << max << endl;
+}
+void shiftLeft(int arr[], const int n)
+{
+	int buffer = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	arr[n - 1] = buffer;
+}
+void shiftRight(int arr[], const int n)
+{
+	int buffer = arr[n - 1];
+
+	for (int i = n - 1; i > 0; i--)
+	{
+		arr[i] = arr[i - 1];
+	}
+	arr[0] = buffer;
+}
---
+++ Functions/main.cpp
@@ -0,0 +1,41 @@
+﻿//Functions
+#include<iostream>
+using namespace std;
+
+int Sum(int a, int b); // Прототип
+int Dif(int a, int b);
+int Product(int a, int b);
+double Quotient(int a, int b);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите два числа: ", cin >> a >> b;
+	int c = Sum(a, b);  // Использование функции (Вызов функции Function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Dif(a, b) << endl;
+	cout << a << " * " << b << " = " << Product(a, b) << endl;
+	cout << a << " / " << b << " = " << Quotient(a, b) << endl;
+}
+int Sum(int a, int b) // Реализация функции (Function defenition)
+{
+	int c = a + b;
+	return c;
+}
+int Dif(int a, int b)
+{
+	//Difference - разность
+	return a - b;
+
+}
+
+int Product(int a, int b)
+{
+	return a * b;
+}
+double Quotient(int a, int b)
+{
+	return (double)a / b;
+}
